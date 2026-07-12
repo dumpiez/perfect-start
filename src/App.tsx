0232ca16d13d9f6871c1bf3434ace2cdeb6d9483
dumpiez <nightlypie@gmail.com>
2026-07-12 16:16:12 +0700

    shortcuts are now saved to localStorage, and also add 'Shortcuts' type

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchBar from "./components/searchbar.tsx";
 import Clock from "./components/clock.tsx";
@@ -6,31 +6,44 @@ import DateTime from "./components/datetime.tsx";
 import Shortcuts from "./components/shortcuts.tsx";
 import Modal from "./components/modal.tsx";
 
+type Shortcut = {
+  name: string;
+  url: string;
+};
+
 const App = () => {
   const [editMode, setEditMode] = useState(false);
   const [modalVisibility, setModalVisibility] = useState(false);
-  const [shortcuts, setShortcuts] = useState([
-    {
-      name: "HackClub",
-      url: "https://hackclub.com",
-    },
-    {
-      name: "Slack",
-      url: "https://slack.com",
-    },
-    {
-      name: "Stardance",
-      url: "https://stardance.hackclub.com",
-    },
-    {
-      name: "YouTube",
-      url: "https://youtube.com",
-    },
-    {
-      name: "Hackatime",
-      url: "https://hackatime.hackclub.com",
-    },
-  ]);
+  const [shortcuts, setShortcuts] = useState<Shortcut[]>(() => {
+    const saved = localStorage.getItem("shortcuts");
+
+    if (!saved) {
+      return [
+        {
+          name: "HackClub",
+          url: "https://hackclub.com",
+        },
+        {
+          name: "Slack",
+          url: "https://slack.com",
+        },
+        {
+          name: "Stardance",
+          url: "https://stardance.hackclub.com",
+        },
+        {
+          name: "YouTube",
+          url: "https://youtube.com",
+        },
+        {
+          name: "Hackatime",
+          url: "https://hackatime.hackclub.com",
+        },
+      ];
+    } else {
+      return JSON.parse(saved);
+    }
+  });
 
   function deleteShortcut(url: string) {
     // filter shortcuts, if result is false then remove, if result is true then don't remove
@@ -44,6 +57,10 @@ const App = () => {
   function closeModal() {
     setModalVisibility(!modalVisibility);
   }
+
+  useEffect(() => {
+    localStorage.setItem("shortcuts", JSON.stringify(shortcuts));
+  }, [shortcuts]);
 
   return (
     <div>
